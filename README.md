7d8e355be017a5b5a236d1860fd757e4d4a65fed
Nicholas Granado <ngranado@gmail.com>
2013-03-07 10:33:52 -0800

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 statsd-go
 =========
+I have implemented gauges ... other implementations do not do this.
+
+## running the example ##
+1. you must install [Temperature Monitor](http://www.bresink.com/osx/TemperatureMonitor.html) to get temp data from OS X.
+1. install statsd-go
+
+	$ go get github.com/heatxsink/statsd-go
+1. run example
+
+	$ cd examples
+	$ go run example.go
+
+## the license ##
+this code is completely free under the mit license: [http://mit-license.org/](http://mit-license.org/).
---
+++ examples/example.go
@@ -0,0 +1,49 @@
+//
+// To run type:
+// $ go run example.go
+//
+package main
+
+import "github.com/heatxsink/statsd-go"
+
+import (
+	"os/exec"
+	"bytes"
+	"log"
+	"strings"
+	"strconv"
+)
+
+type temperature_sensors map[string] string
+
+func get_computer_temperature() temperature_sensors {
+	command := "/Applications/TemperatureMonitor.app/Contents/MacOS/tempmonitor"
+	cmd := exec.Command(command, "-c", "-l", "-a")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	temp_data := temperature_sensors {}
+	lines := strings.Split(out.String(), "\n")
+	for _, line := range lines {
+		datum := strings.Split(line, ": ")
+		if len(datum) > 1 {
+			key := datum[0]
+			value := strings.TrimRight(datum[1], " C")
+			temp_data[key] = value
+		}
+	}
+	return temp_data
+}
+
+func main() {
+	sensor_data :=get_computer_temperature()
+	sensor_key := "SMC CPU A DIODE"
+	sensor_value, _ := strconv.Atoi(sensor_data[sensor_key])
+	hostname := "127.0.0.1"
+	port_number := 9121
+	client := statsd.New(hostname, port_number)
+	client.Gauge("koc.test.smc_cpu_a_diode", sensor_value)
+}
